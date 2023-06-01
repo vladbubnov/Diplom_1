b380d0dcde9b8c2f2067e443411ff579a4693912
Vlad Bubnov <vladbubnov.qa@gmail.com>
2023-06-02 00:24:06 +0200

    fix errors

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,21 +1,47 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 import praktikum.Bun;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class BunTest {
+    private final String name;
+    private final Float price;
 
-    Bun bun = new Bun("Dallas", 5F);
+
+    public BunTest(String name, Float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameters(name = "Тестовые данные: {0}, {1}")
+    public static Object[][] getBunData(){
+        return new Object[][]{
+                {"Флюоресцентная булка R2-D3", 1.5f},
+                {"Б", 2.0f},
+                {"булкааааааааааааааааааааааааааааааааааааааааааааааааааааа", 2.5f},
+                {null, 3.0f},
+                {"Спецсимволы!@#$%^&*()", 3.5f},
+                {"", 4.0f},
+                {"Флюоресцентная булка R2-D3", -2.5f},
+                {"Флюоресцентная булка R2-D3", 0.01f},
+                {"Флюоресцентная булка R2-D3", 4356439857345.87689678f},
+                {"Краторная булка N-200i", 0f}
+        };
+    }
 
     @Test
     public void testBunGetName() {
-        String expectedBunName = "Dallas";
-        assertEquals("Значение теста testBunGetName не совпадают", expectedBunName, bun.getName());
+        Bun bun = new Bun(name, price);
+        assertEquals("Значение теста testBunGetName не совпадают", name, bun.getName());
     }
 
     @Test
     public void testBunGetPrice() {
-        float expectedBunPrice = 5F;
-        assertEquals("Значение теста testBunGetPrice не совпадают", expectedBunPrice, bun.getPrice(), 0);
+        Bun bun = new Bun(name, price);
+        assertEquals("Значение теста testBunGetPrice не совпадают", price, bun.getPrice(), 0);
     }
 }
